f91fac6007389712aab6831300ed92a64be8b32c
Pavel Perkin <perkinpv@gmail.com>
2018-06-10 19:25:10 +0300

    VerticalSergment command

---
+++ lib/commands/vertical_segment.rb
@@ -0,0 +1,39 @@
+require 'command'
+require 'validator'
+
+class VerticalSegment < Command
+  def initialize(state: ,x: ,y1: ,y2: ,colour: )
+    @state = state.to_a
+    @x = x.to_i
+    @y1 = y1.to_i
+    @y2 = y2.to_i
+    @colour = colour.to_s
+    validate!
+  end
+
+  def run()
+    if @y1 >= @y2
+      @state[@y2-1 .. @y1-1].each { |row| row[@x-1] = @colour }
+    else
+      @state[@y1-1 .. @y2-1].each { |row| row[@x-1] = @colour}
+    end
+    @state
+  end
+
+  private
+
+  def validate!
+    raise ArgumentError unless Validator.new(obj: @state, rules: { class: Array, empty?: false }).valid?
+    n = @state.size
+    m = @state.first.size
+    raise ArgumentError unless Validator.new(obj: @x, rules: { integer?: true, positive?: true }).valid?
+    raise ArgumentError unless Validator.new(obj: @x-m, rules: { positive?: false }).valid?
+    raise ArgumentError unless Validator.new(obj: @y1, rules: { integer?: true, positive?: true }).valid?
+    raise ArgumentError unless Validator.new(obj: @y1-n, rules: { positive?: false }).valid?
+    raise ArgumentError unless Validator.new(obj: @y2, rules: { integer?: true, positive?: true }).valid?
+    raise ArgumentError unless Validator.new(obj: @y2-n, rules: { positive?: false }).valid?
+    raise ArgumentError unless Validator.new(obj: @colour, rules: { empty?: false }).valid?
+    raise ArgumentError unless Validator.new(obj: @colour.size - 1, rules: { zero?: true }).valid?
+    raise ArgumentError unless Validator.new(obj: @colour, rules: { capitalize: @colour }).valid?
+  end
+end
---
+++ spec/lib/commands/vertical_segment_spec.rb
@@ -0,0 +1,237 @@
+require_relative "../../../lib/commands/vertical_segment"
+
+RSpec.describe VerticalSegment do
+  describe '#new' do
+    subject { VerticalSegment.new(state: state, x: x, y1: y1, y2: y2, colour: colour) }
+    let(:state) { [['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O']] }
+    let(:x) { 3 }
+    let(:y1) { 1 }
+    let(:y2) { 3 }
+    let(:colour) { 'A' }
+
+    it { is_expected.to be_instance_of(VerticalSegment) }
+    it { is_expected.to be_kind_of(Command) }
+
+    context 'valid attributes' do
+      context 'x == 1' do
+        let(:x) { 1 }
+        it { is_expected.to be_instance_of(VerticalSegment) }
+        it { is_expected.to be_kind_of(Command) }
+      end
+
+      context 'y1 == 1' do
+        let(:y1) { 1 }
+        it { is_expected.to be_instance_of(VerticalSegment) }
+        it { is_expected.to be_kind_of(Command) }
+      end
+
+      context 'y2 == 1' do
+        let(:y2) { 1 }
+        it { is_expected.to be_instance_of(VerticalSegment) }
+        it { is_expected.to be_kind_of(Command) }
+      end
+
+      context 'x == bitmap size' do
+        let(:x) { 3 }
+        it { is_expected.to be_instance_of(VerticalSegment) }
+        it { is_expected.to be_kind_of(Command) }
+      end
+
+      context 'y1 == bitmap size' do
+        let(:y1) { 4 }
+        it { is_expected.to be_instance_of(VerticalSegment) }
+        it { is_expected.to be_kind_of(Command) }
+      end
+
+      context 'y2 == bitmap size' do
+        let(:y2) { 4 }
+        it { is_expected.to be_instance_of(VerticalSegment) }
+        it { is_expected.to be_kind_of(Command) }
+      end
+
+      context 'colour is one capital letter' do
+        let(:colour) { 'K' }
+        it { is_expected.to be_instance_of(VerticalSegment) }
+        it { is_expected.to be_kind_of(Command) }
+      end
+    end
+
+    context 'invalide attributes' do
+      context 'invalid state' do
+        let(:state) { nil }
+        it 'raises an ArgumentError' do
+          expect { subject }.to raise_error(ArgumentError)
+        end
+      end
+
+      context 'empty state' do
+        let(:state) { [] }
+        it 'raises an ArgumentError' do
+          expect { subject }.to raise_error(ArgumentError)
+        end
+      end
+
+      context 'invalid coordinates' do
+        context 'x < 0 ' do
+          let(:x) { -1 }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'y1 < 0 ' do
+          let(:y1) { -1 }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'y2 < 0 ' do
+          let(:y2) { -1 }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'x == 0 ' do
+          let(:x) { 0 }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'y1 == 0 ' do
+          let(:y1) { 0 }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'y2 == 0 ' do
+          let(:y2) { 0 }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'x > bitmap size ' do
+          let(:x) { 4 }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'y1 > bitmap size' do
+          let(:y1) { 5 }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'y2 > bitmap size' do
+          let(:y2) { 5 }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'colour is nil' do
+          let(:colour) { nil }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'colour is empty' do
+          let(:colour) { '' }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'colour is longer than one char' do
+          let(:colour) { 'AB' }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+
+        context 'colour is a lowercase char' do
+          let(:colour) { 'a' }
+          it 'raises an ArgumentError' do
+            expect { subject }.to raise_error(ArgumentError)
+          end
+        end
+      end
+    end
+  end
+
+  describe '#run' do
+    subject { VerticalSegment.new(state: state, x: x, y1: y1, y2: y2, colour: colour).run }
+    let(:state) { [['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O']] }
+    let(:colour) { 'A' }
+
+    context 'left top corner' do
+      let(:x) { 1 }
+      let(:y1) { 1 }
+      let(:y2) { 1 }
+      it { is_expected.to match [['A', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O']] }
+    end
+
+    context 'right top corner' do
+      let(:x) { 3 }
+      let(:y1) { 1 }
+      let(:y2) { 1 }
+      it { is_expected.to match [['O', 'O', 'A'], ['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O']] }
+    end
+
+    context 'left bottom corner' do
+      let(:x) { 3 }
+      let(:y1) { 4 }
+      let(:y2) { 4 }
+      it { is_expected.to match [['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'A']] }
+    end
+
+    context 'right bottom corner' do
+      let(:x) { 1 }
+      let(:y1) { 4 }
+      let(:y2) { 4 }
+      it { is_expected.to match [['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O'], ['A', 'O', 'O']] }
+    end
+
+    context 'left line' do
+      let(:x) { 1 }
+      let(:y1) { 1 }
+      let(:y2) { 4 }
+      it { is_expected.to match [['A', 'O', 'O'], ['A', 'O', 'O'], ['A', 'O', 'O'], ['A', 'O', 'O']] }
+    end
+
+    context 'left line 2' do
+      let(:x) { 1 }
+      let(:y1) { 4 }
+      let(:y2) { 1 }
+      it { is_expected.to match [['A', 'O', 'O'], ['A', 'O', 'O'], ['A', 'O', 'O'], ['A', 'O', 'O']] }
+    end
+
+    context 'right line' do
+      let(:x) { 3 }
+      let(:y1) { 1 }
+      let(:y2) { 4 }
+      it { is_expected.to match [['O', 'O', 'A'], ['O', 'O', 'A'], ['O', 'O', 'A'], ['O', 'O', 'A']] }
+    end
+
+    context 'right line 2' do
+      let(:x) { 3 }
+      let(:y1) { 4 }
+      let(:y2) { 1 }
+      it { is_expected.to match [['O', 'O', 'A'], ['O', 'O', 'A'], ['O', 'O', 'A'], ['O', 'O', 'A']] }
+    end
+
+    context 'segment' do
+      let(:x) { 2 }
+      let(:y1) { 3 }
+      let(:y2) { 2 }
+      it { is_expected.to match [['O', 'O', 'O'], ['O', 'A', 'O'], ['O', 'A', 'O'], ['O', 'O', 'O']] }
+    end
+  end
+end
